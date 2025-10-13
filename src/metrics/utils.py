6baadd6c1632431e4410244766b59bc6a644f1f5
IvanPodoynikov <ipodoynikov@yandex.ru>
2025-10-14 00:13:35 +0300

    cer wer

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -7,10 +7,10 @@ from torch import Tensor
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    return editdistance.eval(target_text, predicted_text) / max(1, len(target_text))
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
     target_words = target_text.split()
     predicted_words = predicted_text.split()
-    return editdistance.eval(target_words, predicted_words) / max(1, len(target_words))
+    return editdistance.eval(target_words, predicted_words) / len(target_words)
