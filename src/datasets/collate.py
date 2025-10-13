0d235fabad3e79c26dbefe710c1dbadc655f8f58
IvanPodoynikov <ipodoynikov@yandex.ru>
2025-10-13 20:20:51 +0300

    пробую заново запустить torch deepspeech

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn.utils.rnn import pad_sequence
 
 
 def collate_fn(dataset_items: list[dict]):
@@ -14,4 +15,28 @@ def collate_fn(dataset_items: list[dict]):
             of the tensors.
     """
 
-    pass  # TODO
+    def _strip_leading_channel(key: str, t: torch.Tensor):
+        if key == "spectrogram":
+            return t.squeeze(0).transpose(0, 1)  # (1, F, T) -> (T, F)
+        elif key == "text_encoded":
+            return t.squeeze(0)
+        return t
+
+    result_batch = {}
+    keys = list(dataset_items[0].keys())
+    for key in keys:
+        values = [_strip_leading_channel(key, item[key]) for item in dataset_items]
+        if key in ["spectrogram", "text_encoded"]:
+            result_batch[key] = pad_sequence(values, batch_first=True, padding_value=0)
+            if key == "spectrogram":
+                result_batch[key] = result_batch[key].transpose(1, 2)  # (B, F, T)
+        else:
+            result_batch[key] = values  # keep as list (e.g., strings, paths)
+
+    result_batch["spectrogram_length"] = torch.Tensor(
+        [item.shape[-1] for item in result_batch["spectrogram"]]
+    ).long()
+    result_batch["text_encoded_length"] = torch.Tensor(
+        [item.shape[-1] for item in result_batch["text_encoded"]]
+    ).long()
+    return result_batch
--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,16 @@
 # Based on seminar materials
 
 # Don't forget to support cases when target_text == ''
+import editdistance
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    assert target_text != "", "Target text is empty!"
+    return editdistance.eval(target_text, predicted_text) / max(1, len(target_text))
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    assert target_text != "", "Target text is empty!"
+    target_words = target_text.split()
+    predicted_words = predicted_text.split()
+    return editdistance.eval(target_words, predicted_words) / max(1, len(target_words))
