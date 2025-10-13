3d6771facbf0287f741cf64d179018ffbc2b3f25
IvanPodoynikov <ipodoynikov@yandex.ru>
2025-10-13 21:03:37 +0300

    смена utils

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,16 +1,16 @@
-# Based on seminar materials
+from typing import List
+
+import editdistance
+from torch import Tensor
 
 # Don't forget to support cases when target_text == ''
-import editdistance
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    assert target_text != "", "Target text is empty!"
-    return editdistance.eval(target_text, predicted_text) / max(1, len(target_text))
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    assert target_text != "", "Target text is empty!"
     target_words = target_text.split()
     predicted_words = predicted_text.split()
-    return editdistance.eval(target_words, predicted_words) / max(1, len(target_words))
+    return editdistance.eval(target_words, predicted_words) / len(target_words)
