d02be6c4df47950f1fe8fd51c322b89cb00a9b9f
IvanPodoynikov <ipodoynikov@yandex.ru>
2025-10-14 01:36:58 +0300

    ctc_loss ыъыъыъыъыъ

--- src/loss/ctc_loss.py
+++ src/loss/ctc_loss.py
@@ -4,17 +4,18 @@ from torch.nn import CTCLoss
 
 
 class CTCLossWrapper(CTCLoss):
+    def __init__(self, *args, zero_infinity: bool = True, **kwargs):
+        super().__init__(*args, zero_infinity=zero_infinity, **kwargs)
+
     def forward(
         self, log_probs, log_probs_length, text_encoded, text_encoded_length, **batch
-    ) -> Tensor:
-        log_probs_t = torch.transpose(log_probs, 0, 1)
-
+    ):
+        log_probs_t = log_probs.transpose(0, 1)
         loss = super().forward(
             log_probs=log_probs_t,
             targets=text_encoded,
             input_lengths=log_probs_length,
             target_lengths=text_encoded_length,
-            zero_infinity=True,
         )
 
         return {"loss": loss}
