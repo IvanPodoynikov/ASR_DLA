2b929aa6f78fbe94b6783056c29e17c489b017a8
IvanPodoynikov <ipodoynikov@yandex.ru>
2025-10-13 21:15:28 +0300

    фикс

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -7,10 +7,12 @@ from torch import Tensor
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    return editdistance.eval(target_text, predicted_text) / len(target_text)
+    assert target_text != "", "Target text is empty"
+    return editdistance.eval(target_text, predicted_text) / max(1, len(target_text))
 
 
 def calc_wer(target_text, predicted_text) -> float:
+    assert target_text != "", "Target text is empty"
     target_words = target_text.split()
     predicted_words = predicted_text.split()
-    return editdistance.eval(target_words, predicted_words) / len(target_words)
+    return editdistance.eval(target_words, predicted_words) / max(1, len(target_words))
