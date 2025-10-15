6003b928f3de82b6e13d7c1ca32afec93da36b45
IvanPodoynikov <ipodoynikov@yandex.ru>
2025-10-16 01:53:05 +0300

    ауги

--- src/transforms/wav_augs/addcolorednoise.py
+++ src/transforms/wav_augs/addcolorednoise.py
@@ -2,10 +2,10 @@ import torch_audiomentations
 from torch import Tensor, nn
 
 
-class PolarityInversion(nn.Module):
+class AddColoredNoise(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__()
-        self._aug = torch_audiomentations.PolarityInversion(*args, **kwargs)
+        self._aug = torch_audiomentations.AddColoredNoise(*args, **kwargs)
 
     def __call__(self, data: Tensor):
         x = data.unsqueeze(1)
--- src/transforms/wav_augs/pitchshift.py
+++ src/transforms/wav_augs/pitchshift.py
@@ -3,13 +3,9 @@ from torch import Tensor, nn
 
 
 class PitchShift(nn.Module):
-    def __init__(
-        self, min_semitones: float = -4.0, max_semitones: float = 4.0, p: float = 0.5
-    ):
+    def __init__(self, *args, **kwargs):
         super().__init__()
-        self._aug = torch_audiomentations.PitchShift(
-            min_semitones=min_semitones, max_semitones=max_semitones, p=p
-        )
+        self._aug = torch_audiomentations.PitchShift(*args, **kwargs)
 
     def __call__(self, data: Tensor):
         x = data.unsqueeze(1)
